c466bf5519a4960f44a9ae4c009a79b510af28fb
RickyMed <ricardo.med.gonzalez@gmail.com>
2019-07-03 05:29:09 -0700

    Mostly Responsive

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,25 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-<<<<<<< HEAD
     <footer id="main-footer" className="is-center py-1">
       <div className="container">
         <p>Copyright &copy; 2019, Marble Specialist, All Rights Reserved.</p>
-=======
-    <footer id="main-footer">
-      <div className="footer-nav p-1">
-        <nav role="navigation">
-          <ul className="">
-            <li><a href="#menu">Catering</a></li>
-            <li><a href="#locations">Rewards</a></li>
-            <li><a href="#about">Careers</a></li>
-          </ul>
-        </nav>
-      </div>
-      <hr/>
-      <div className="footer-content is-center">
-        <p className="s-lead">Copyright &copy; 2019, La Bufadora Baja Grill, All Rights Reserved.</p>
->>>>>>> 22528944898d7519061f4ea1ee4bd3b67d8cad45
       </div>
     </footer>
   )
