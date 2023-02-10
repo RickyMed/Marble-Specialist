337f67dc49156fbb321a8b44f545dbf8a2066020
RickyMed <Ricardo.med.gonzalez@gmail.com>
2023-02-09 17:15:06 -0800

    Update Images

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -15,7 +15,7 @@ const Gallery = () => {
           <img src="https://images.pexels.com/photos/2117937/pexels-photo-2117937.jpeg?cs=srgb&dl=concrete-floor-granite-2117937.jpg&fm=jpg" alt="Stone-3" className="gallery-img"/>
         </figure>
         <figure className="gallery-item gallery-item-4">
-          <img src="https://images.pexels.com/photos/921776/pexels-photo-921776.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Stone-4" className="gallery-img"/>
+          <img src="https://images.pexels.com/photos/247719/pexels-photo-247719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Stone-4" className="gallery-img"/>
         </figure>
         <figure className="gallery-item gallery-item-5">
           <img src="https://images.pexels.com/photos/2109758/pexels-photo-2109758.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Stone-5" className="gallery-img"/>
@@ -24,7 +24,7 @@ const Gallery = () => {
           <img src="https://images.pexels.com/photos/1451474/pexels-photo-1451474.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Stone-6" className="gallery-img"/>
         </figure>
         <figure className="gallery-item gallery-item-7">
-          <img src="https://images.pexels.com/photos/1573806/pexels-photo-1573806.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Stone-7" className="gallery-img"/>
+          <img src="https://images.pexels.com/photos/247719/pexels-photo-247719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Stone-7" className="gallery-img"/>
         </figure>
       </div>
     </section>
